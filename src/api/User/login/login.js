e10c6c97f5697d4019db2741f3c84002e098145d
juraffe <cd80@kakao.com>
2020-11-02 12:42:03 +0900

    🍭 로그인 API 수정
    Query -> Mutation 으로 변경 및 최근 로그인 시간 기록

--- src/api/User/login/login.js
+++ src/api/User/login/login.js
@@ -3,12 +3,20 @@ import { prisma } from "../../../../generated/prisma-client";
 import { generateWebToken } from "../../../utils";
 
 export default {
-  Query: {
+  Mutation: {
     login: async (_, args) => {
       const { username, password } = args;
       const user = await prisma.user({ username });
       const isValid = await argon2.verify(user.password, password);
       if (!!isValid) {
+        await prisma.updateUser({
+          where: {
+            username,
+          },
+          data: {
+            loggedInAt: new Date().toISOString().slice(0, 10),
+          },
+        });
         return generateWebToken(user.id);
       } else {
         throw Error("Incorrect username or password");
