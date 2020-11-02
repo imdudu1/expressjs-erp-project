e8e762fb6215fc24aa80566af300b1ace1ab9202
juraffe <cd80@kakao.com>
2020-11-02 12:45:01 +0900

    🍭 부서 가입 API 제거
    users 필드 제거로 인한 joinDepartment 제거

--- src/api/Department/Department.js
+++ src/api/Department/Department.js
@@ -3,6 +3,5 @@ import { prisma } from "../../../generated/prisma-client";
 export default {
   Department: {
     leaderUser: ({ id }) => prisma.department({ id }).leaderUser(),
-    users: ({ id }) => prisma.department({ id }).users(),
   },
 };
