4a960582f817def949aac71843f92bf5bcd7a0cc
juraffe <cd80@kakao.com>
2020-11-15 22:11:05 +0900

    📧 메일 목록 오름차순 정렬

--- src/api/Mail/searchMail/searchMail.js
+++ src/api/Mail/searchMail/searchMail.js
@@ -37,6 +37,7 @@ export default {
             },
           ],
         },
+        orderBy: "createdAt_DESC",
       });
     },
   },
--- src/api/Mail/seeMail/seeMail.js
+++ src/api/Mail/seeMail/seeMail.js
@@ -19,6 +19,7 @@ export default {
             },
           ],
         },
+        orderBy: "createdAt_DESC",
       });
     },
   },
