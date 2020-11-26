7f763d84c663f77fbde39c63dd9aaa954a9ff2a7
juraffe <cd80@kakao.com>
2020-11-27 00:30:39 +0900

    📃 금일 업무 일지 중복 생성 방지

--- src/api/DailyJournal/createDailyJournal/createDailyJournal.js
+++ src/api/DailyJournal/createDailyJournal/createDailyJournal.js
@@ -1,10 +1,28 @@
 import { prisma } from "../../../../generated/prisma-client";
+import { format, subHours } from "date-fns";
 
 export default {
   Mutation: {
     createDailyJournal: async (_, args, { request, isAuthenticated }) => {
       const user = await isAuthenticated(request);
 
+      const today = subHours(new Date(format(new Date(), "yyyy-MM-dd")), 9);
+      const tomorrow = subHours(new Date(today), 9);
+      tomorrow.setDate(today.getDate() + 1);
+      const exists = await prisma.$exists.dailyJournal({
+        AND: [
+          {
+            createdAt_gte: today
+          },
+          {
+            createdAt_lt: tomorrow
+          }
+        ]
+      });
+      console.log(exists);
+      if (exists) {
+        return null;
+      }
       return prisma.createDailyJournal({
         user: {
           connect: {
