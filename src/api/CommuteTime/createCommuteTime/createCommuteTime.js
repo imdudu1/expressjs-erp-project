349f523d0652ef7271e8073acaee386651f9a244
juraffe <cd80@kakao.com>
2020-11-29 09:40:06 +0900

    ⌚ 출퇴 기록 API 작성

--- src/api/CommuteTime/createCommuteTime/createCommuteTime.js
+++ src/api/CommuteTime/createCommuteTime/createCommuteTime.js
@@ -1,12 +1,37 @@
+import { addHours, startOfDay, differenceInHours } from "date-fns";
+import { prisma } from "../../../../generated/prisma-client";
+
 export default {
   Mutation: {
     createCommuteTime: async (_, args, { request, isAuthenticated }) => {
       const user = await isAuthenticated(request);
       const { startDate, endDate } = args;
-      let start = new Date(startDate);
-      let end = new Date(endDate);
-      let workTime = (end.getTime() - start.getTime()) / (1000 * 60 * 60);
-      let pay = workTime * process.env.MINIMUM_WAGE;
-    },
-  },
+      const s = new Date(startDate);
+      const e = new Date(endDate);
+
+      let dayShift = differenceInHours(e, s);
+      let nightShift = differenceInHours(e, addHours(startOfDay(s), 22));
+      if (nightShift <= 0) {
+        nightShift = 0;
+      }
+      let overtime = dayShift - 8;
+      if (overtime <= 0) {
+        overtime = 0;
+      }
+      dayShift -= overtime;
+
+      return prisma.createCommuteTime({
+        user: {
+          connect: {
+            id: user.id
+          }
+        },
+        workDateTime: new Date(startDate),
+        offWorkDateTime: new Date(endDate),
+        workTime: dayShift,
+        overWorkTime: overtime,
+        nightShiftTime: nightShift
+      });
+    }
+  }
 };
