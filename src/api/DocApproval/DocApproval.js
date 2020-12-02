7a4ee4ec25ae73dc2527219bbd2d432a13c75fac
juraffe <cd80@kakao.com>
2020-12-02 12:58:27 +0900

    📝 결재자 순서를 위한 정렬

--- src/api/DocApproval/DocApproval.js
+++ src/api/DocApproval/DocApproval.js
@@ -3,7 +3,8 @@ import { prisma } from "../../../generated/prisma-client";
 export default {
   DocApproval: {
     currentApprover: ({ id }) => prisma.docApproval({ id }).currentApprover(),
-    approvers: ({ id }) => prisma.docApproval({ id }).approvers(),
+    approvers: ({ id }) =>
+      prisma.docApproval({ id }).approvers({ orderBy: "createdAt_DESC" }),
     reviewers: ({ id }) => prisma.docApproval({ id }).reviewers(),
     drafter: ({ id }) => prisma.docApproval({ id }).drafter(),
   },
